f146266507f9d8f90c676ccec85e26c8a483feca
Tommy Kargul <Tommy@tommys-macbook-pro.local>
2016-02-03 22:31:30 -0500

    upload entire project

--- PictureViewer/DetailViewController.swift
+++ PictureViewer/DetailViewController.swift
@@ -1,45 +1,55 @@
 //
 //  DetailViewController.swift
-//  PictureViewer
+//  StormViewer
 //
-//  Created by Tommy Kargul on 2/3/16.
+//  Created by Tommy Kargul on 2/2/16.
 //  Copyright © 2016 Tommy Kargul. All rights reserved.
 //
 
 import UIKit
 
 class DetailViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
-    var detailItem: AnyObject? {
+    
+    @IBOutlet weak var detailImageView: UIImageView!
+    
+    var detailItem: String? {
         didSet {
             // Update the view.
             self.configureView()
         }
     }
-
+    
     func configureView() {
         // Update the user interface for the detail item.
         if let detail = self.detailItem {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.description
+            if let imageView = self.detailImageView {
+                imageView.image = UIImage(named: detail)
             }
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
     }
-
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
 
